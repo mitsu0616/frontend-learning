809e604ef4b931d10834c217ce52dd4c10159a4d
mitsu <mitsu@sample.com>
2023-05-30 23:17:42 +0900

    枠組み作成

--- src/views/ProgressSteps.vue
+++ src/views/ProgressSteps.vue
@@ -1,7 +1,17 @@
-<script setup></script>
+<script setup>
+import StepBar from "../components/ProgressSteps/StepBar.vue"
+import Button from "../components/ProgressSteps/Button.vue"
+</script>
 
 <template>
   <h1>Progress Steps</h1>
+  <div class="page">
+    <div class="container">
+      <StepBar />
+      <Button />
+      <Button />
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
