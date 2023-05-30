c15a43b7a70ca5a3f049d455e523f0a9004f6642
mitsu <mitsu@sample.com>
2023-05-30 23:13:56 +0900

    ProgressSteps ルーティング設定追加

--- src/route/index.js
+++ src/route/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Top from "../views/Top.vue";
 import ExpandingCards from "../views/ExpandingCards.vue";
+import ProgressSteps from "../views/ProgressSteps.vue"
 
 const routes = [
   { path: "/", component: Top },
   { path: "/expanding-cards", component: ExpandingCards },
+  { path: "/progress-steps", component: ProgressSteps}
 ];
 
 const router = createRouter({
