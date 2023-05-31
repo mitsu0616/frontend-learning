0c6980f342cea5c036e86e09632d6669e4642f6d
mitsu <mitsu@sample.com>
2023-05-31 12:42:45 +0900

    状態マーク作成

--- src/components/ProgressSteps/StepBar.vue
+++ src/components/ProgressSteps/StepBar.vue
@@ -1,7 +1,15 @@
-<script setup></script>
+<script setup>
+import StepCircle from "./StepCircle.vue"
+</script>
 
 <template>
-  
+  <div class="container">
+    <div class="progress"></div>
+    <StepCircle number="1" active="true"/>
+    <StepCircle number="2"/>
+    <StepCircle number="3"/>
+    <StepCircle number="4"/>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/ProgressSteps/StepCircle.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  number: Number,
+  active: Boolean,
+});
+
+</script>
+
+<template>
+  <div class="circle" :class="{ active: props.active }">{{ props.number }}</div>
+</template>
+
+<style lang="scss" scoped>
+$line-border-fill: #3498db;
+$line-border-empty: #e0e0e0;
+.circle {
+  background-color: #fff;
+  color: #999;
+  border-radius: 50%;
+  height: 30px;
+  width: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 3px solid $line-border-empty;
+  transition: 0.4s ease;
+
+  &.active {
+    border-color: $line-border-fill;
+  }
+}
+</style>
