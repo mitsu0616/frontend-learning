e910c7ed9d5bf374ffdab4bd3cceed23bc667b4b
mitsu <mitsu@sample.com>
2023-06-01 19:11:09 +0900

    HeadNavによる画面の回転処理実装

--- src/store/rotatingNavAnimation.js
+++ src/store/rotatingNavAnimation.js
@@ -1,5 +1,15 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
-export const useRotatingNavAnimationStore = defineStore("rotatingNavAnimation", () => {
-});
+export const useRotatingNavAnimationStore = defineStore(
+  "rotatingNavAnimation",
+  () => {
+    const show = ref(false);
+
+    const changeShow = () => {
+      show.value = !(show.value)
+    }
+
+    return { show, changeShow };
+  }
+);
--- src/views/RotatingNavAnimation.vue
+++ src/views/RotatingNavAnimation.vue
@@ -1,7 +1,42 @@
-<script setup></script>
+<script setup>
+import HeadNav from "../components/RotatingNavAnimation/HeadNav.vue";
+import Content from "../components/RotatingNavAnimation/Content.vue";
+import BottomNav from "../components/RotatingNavAnimation/BottomNav.vue";
+
+import { useRotatingNavAnimationStore } from "../store/rotatingNavAnimation";
+
+const store = useRotatingNavAnimationStore();
+</script>
 
 <template>
-  <fa icon="bars" />
+  <div class="page">
+    <div class="container" :class="{show: store.show}">
+      <HeadNav />
+      <Content />
+      <BottomNav />
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page {
+  box-sizing: border-box;
+  background-color: #333;
+  color: #222;
+  overflow-x: hidden;
+  margin: 0;
+  .container {
+    background-color: #fafafa;
+    // transform-origin: 変形させる中心点の位置、下記だと「左上」が中心点になる
+    transform-origin: top left;
+    transition: transform 0.5s linear;
+    width: 100vw;
+    min-height: 100vh;
+    padding: 50px;
+
+    &.show {
+      transform: rotate(-20deg);
+    }
+  }
+}
+</style>
