680757ffa661830b68424e8210cdfb7a933666fc
mitsu0616 <83687858+mitsu0616@users.noreply.github.com>
2023-06-02 20:10:31 +0900

    Revert "Project3"

--- src/main.js
+++ src/main.js
@@ -5,15 +5,9 @@ import "./reset.css";
 import App from "./App.vue";
 import router from "./route";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-library.add(fas);
-
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(router);
 app.use(pinia);
-app.component("fa", FontAwesomeIcon);
 app.mount("#app");
--- src/route/index.js
+++ src/route/index.js
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Top from "../views/Top.vue";
 import ExpandingCards from "../views/ExpandingCards.vue";
-import ProgressSteps from "../views/ProgressSteps.vue";
-import RotatingNavAnimation from "../views/RotatingNavAnimation.vue";
+import ProgressSteps from "../views/ProgressSteps.vue"
 
 const routes = [
   { path: "/", component: Top },
   { path: "/expanding-cards", component: ExpandingCards },
-  { path: "/progress-steps", component: ProgressSteps },
-  { path: "/rotating-nav-animation", component: RotatingNavAnimation },
+  { path: "/progress-steps", component: ProgressSteps}
 ];
 
 const router = createRouter({
